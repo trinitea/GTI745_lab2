c4fb7b5283f79bdda7a931c87f76dc1311dcc07b
DESKTOP-ELGJ1VA\Level5-Chan <chan.jpeg@gmail.com>
2017-06-09 17:22:27 -0400

    Difficulty Options/Game Controller Options Done.

--- Assets/Script/Utility/GameModel.cs
+++ Assets/Script/Utility/GameModel.cs
@@ -2,5 +2,6 @@
 
 public class GameModel
 {
-    static public Difficulty gameDifficulty = Difficulty.Normal;
+    static public Difficulty gameDifficulty = Difficulty.Normal;            //Difficulty is in DifficultySettings.cs
+    static public ControllerMode controllerMode = ControllerMode.Tactile;   //Controller Mode is in MainMenuSettings.cs
 }
